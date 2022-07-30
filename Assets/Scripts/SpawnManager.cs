ec8ab0b99ff40a6d4e8a327400966ef35f6ec599
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-07-30 17:31:51 +0100

    Fixed enemy spawner coroutine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,31 +9,23 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] float waitToSpawn = 2f;
 
     private int bossCount = 0;
-    private IEnumerator coroutine;
 
     void Start()
     {
-        SpawnEnemyWave();
+        StartCoroutine(SpawnEnemyWave());
     }
 
     void Update()
     {
         SpawnNextWave();
-    }  
-
-    IEnumerator SpawnEnemy(int index)
-    {        
-        Instantiate(enemies[index], transform.position, enemies[index].transform.rotation);
-        yield return new WaitForSeconds(waitToSpawn);
     }
 
-    void SpawnEnemyWave()
+    IEnumerator SpawnEnemyWave()
     {
         for (int i = 0; i < waveNumber; i++)
         {
-            coroutine = SpawnEnemy(i);
-            StartCoroutine(coroutine);
-            StopCoroutine(coroutine);
+            Instantiate(enemies[i % 4], transform.position, transform.rotation);
+            yield return new WaitForSeconds(waitToSpawn);
         }
     }
 
@@ -42,7 +34,7 @@ public class SpawnManager : MonoBehaviour
         if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
         {
             waveNumber++;
-            SpawnEnemyWave();
+            StartCoroutine(SpawnEnemyWave());
         }
     }
 }
